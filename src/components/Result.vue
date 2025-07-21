f214be1fcd63805bfa6d5764a5fb03484dd175f3
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2025-07-21 18:40:37 +0200

    minor fix

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -9,15 +9,15 @@ const { correct,total } = defineProps(['correct','total']);
              <h3>BIFF</h3>
              <p> Your knowledge about BTTF is poor and confused, just like Biff and his pal, so stop to be arrogant and go see the films again and again!</p>
         </div>
-        <div v-else-if="correct >=6">
+        <div v-else-if="correct >= 9 && correct < 10">
             <img alt="marty "width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fviapais.com.ar%2Fresizer%2Fz-vm8dl5EBvMfFto7_6dlYHRQ8Y%3D%2F980x640%2Fsmart%2Ffilters%3Aquality(75)%3Aformat(webp)%2Fcloudfront-us-east-1.images.arcpublishing.com%2Fgrupoclarin%2F2MQ7XOSVXNACRFTUO64RCAK2RA.jpg&f=1&nofb=1&ipt=f2f1dfbb48d2109796710c74ccaddfa5de9f9bc0bb8789d21ae68071325b3e57"/>
               <h3>MARTY MCFLY</h3>
               <p> Congrats! You made it well, from 6 to 9 correct answers! Your knowledge of BTTF is very good but not the best, make it perfect by watching the trilogy once or twice, It'll be better!</p>
         </div>
-        <div v-else>
+        <div v-els="correct == 10">
             <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.images.express.co.uk%2Fimg%2Fdynamic%2F36%2F590x%2Fsecondary%2Fdoc-brown-2015-2300497.jpg%3Fr%3D1581067400804&f=1&nofb=1&ipt=ad0d27a4338347a865ad2f8c97918e8d191ac98584bcae3d99ef01af897e28fe" />
                <h3>DOC BROWN</h3>
-               <p>GREAT SCOTT! You're awesome! Seems like you wrote the three films by yourself! 10/10! You are the BTTF Genius, like DOC!</p>
+               <p>GREAT SCOTT! You're awesome! Seems like you wrote the three films by yourself! 10/10! You are the BTTF Genius, just like DOC!</p>
         </div>    
           <h1>
             {{ correct }}/{{ total }}
