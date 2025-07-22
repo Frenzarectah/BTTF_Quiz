acac2348b3795991e3053b987b2fd8cea90c0c75
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2025-07-22 10:16:35 +0200

    changed title and favicon

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -5,19 +5,19 @@ const { correct,total } = defineProps(['correct','total']);
     <div class="results">
         <p>Your results...</p>
         <div v-if="correct <= 5">
-            <img alt="biff" width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcostumerocket.com%2Fwp-content%2Fuploads%2F2023%2F01%2Fbiff-tannen-costume.jpg&f=1&nofb=1&ipt=cfe86b0cd274f0c9df41838b3a4c6b3a1ff93b5206c049fb78f0eb2b5587463a"/>
+            <img alt="biff" width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnekorandom.files.wordpress.com%2F2021%2F05%2Fe208a-back-to-the-future-biff-tannen-1955.jpg&f=1&nofb=1&ipt=07141d6dc3c93f6ff3b7875865933733dcaab5e463ba6dac6fa22b1e874a7d67">     
              <h3>BIFF</h3>
              <p> Your knowledge about BTTF is poor and confused, just like Biff and his pal, so stop to be arrogant and go see the films again and again!</p>
         </div>
         <div v-else-if="correct >= 9 && correct < 10">
-            <img alt="marty "width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fviapais.com.ar%2Fresizer%2Fz-vm8dl5EBvMfFto7_6dlYHRQ8Y%3D%2F980x640%2Fsmart%2Ffilters%3Aquality(75)%3Aformat(webp)%2Fcloudfront-us-east-1.images.arcpublishing.com%2Fgrupoclarin%2F2MQ7XOSVXNACRFTUO64RCAK2RA.jpg&f=1&nofb=1&ipt=f2f1dfbb48d2109796710c74ccaddfa5de9f9bc0bb8789d21ae68071325b3e57"/>
+            <img alt="marty "width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jacketsmasters.com%2Fwp-content%2Fuploads%2F2022%2F10%2Fback-to-the-future-marty-mcfly-red-vest.jpg&f=1&nofb=1&ipt=75e255b292f66618cba40f5a8fd33c1d14429881105a5fd9dcc26d8166a07c2c"/>
               <h3>MARTY MCFLY</h3>
               <p> Congrats! You made it well, from 6 to 9 correct answers! Your knowledge of BTTF is very good but not the best, make it perfect by watching the trilogy once or twice, It'll be better!</p>
         </div>
         <div v-else="correct == 10">
-            <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.images.express.co.uk%2Fimg%2Fdynamic%2F36%2F590x%2Fsecondary%2Fdoc-brown-2015-2300497.jpg%3Fr%3D1581067400804&f=1&nofb=1&ipt=ad0d27a4338347a865ad2f8c97918e8d191ac98584bcae3d99ef01af897e28fe" />
+            <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcostumerocket.com%2Fwp-content%2Fuploads%2F2023%2F01%2Fdoc-brown-costume.jpg&f=1&nofb=1&ipt=049d37392422e619b4e54ae38da39087fa8a63e39dc3befaff9161f9977444aa">
                <h3>DOC BROWN</h3>
-               <p>GREAT SCOTT! You're awesome! Seems like you wrote the three films by yourself! 10/10! You are the BTTF Genius, just like DOC!</p>
+               <p>GREAT SCOTT! You're awesome! Seems like you wrote the three films by yourself! 10/10! You are a BTTF Genius, just like DOC!</p>
         </div>    
           <h1>
             {{ correct }}/{{ total }}
@@ -28,6 +28,7 @@ const { correct,total } = defineProps(['correct','total']);
 <style scoped>
 * {
   box-sizing: border-box;
+  background-color: #fffaff;
 }
 div{
     display:flex;
@@ -48,6 +49,9 @@ p{
 }
 h1{
     font-size:80px;
+}
+img{
+    border-radius:50%;
 }
 @media screen and (max-height: 900px) {
     p{
